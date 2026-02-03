821c76dfec3e4410b40b084165475b6da483474c
Waleed-Ahmad-dev <itswaleedqureshi@gmail.com>
2026-02-03 12:27:44 +0500

    feat(app): add config module
    * Create ConfigModule class
    * Import ConfigModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EpisodesModule } from './episodes/episodes.module';
 import { TopicsModule } from './topics/topics.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [EpisodesModule, TopicsModule],
+  imports: [EpisodesModule, TopicsModule, ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
