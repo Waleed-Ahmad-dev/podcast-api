3ec5df6bd8ab4fa081889440b8f9ad694afaf578
Waleed-Ahmad-dev <itswaleedqureshi@gmail.com>
2026-02-03 12:34:40 +0500

    feat(episodes): add findAll endpoint
    * Import Get decorator from @nestjs/common
    * Add @Get() decorator to findAll method in EpisodesController

--- src/episodes/episodes.controller.ts
+++ src/episodes/episodes.controller.ts
@@ -1,7 +1,8 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('episodes')
 export class EpisodesController {
+    @Get()
     findAll() {
         return "All Episodes"
     }
