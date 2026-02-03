d8e31c953d44a7c4960b2bc8c5b9adc43f524ede
Waleed-Ahmad-dev <itswaleedqureshi@gmail.com>
2026-02-03 12:36:56 +0500

    feat(episodes): add create endpoint
    * Import Post decorator from @nestjs/common
    * Add @Post() decorator to create method in EpisodesController
    * Implement create method to return new episode response

--- src/episodes/episodes.controller.ts
+++ src/episodes/episodes.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('episodes')
 export class EpisodesController {
     @Get()
     findAll() {
         return "All Episodes"
+    }
+
+    @Post()
+    create() {
+        return "New Episodes"
     }
 }
