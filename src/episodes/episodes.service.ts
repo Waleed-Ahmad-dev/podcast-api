15a5d9bd425ff62e55ea9b6b4ef098cb71ee176b
Waleed-Ahmad-dev <itswaleedqureshi@gmail.com>
2026-02-03 13:51:24 +0500

    feat(episodes): add find featured method
    * Introduce findFeatured method to retrieve featured episodes
    * Implement logic to filter episodes by featured property

--- src/episodes/episodes.service.ts
+++ src/episodes/episodes.service.ts
@@ -19,4 +19,8 @@ export class EpisodesService {
         console.log(id);
         return this.episodes.find(episode => episode.id === id )
     }
+
+    findFeatured(){
+        return this.episodes.filter(episode => episode.featured === true)
+    }
 }
