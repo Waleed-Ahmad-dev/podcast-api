03eae1d33c01207e04a74e4ef63a149a8ff3a934
Waleed-Ahmad-dev <itswaleedqureshi@gmail.com>
2026-02-03 13:17:57 +0500

    feat(episodes): init episodes array
    * Initialize episodes array in EpisodesService

--- src/episodes/episodes.service.ts
+++ src/episodes/episodes.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Episode } from './episode.entity';
 @Injectable()
 export class EpisodesService {
-    private episodes: Episode[]
+    private episodes: Episode[] = [];
 
     findAll(){
         return this.episodes
