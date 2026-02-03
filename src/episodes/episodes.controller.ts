42a257a6373a7968833182f2d8f34a0110afdfd3
Waleed-Ahmad-dev <itswaleedqureshi@gmail.com>
2026-02-03 12:49:17 +0500

    feat(episodes): add endpoints
    * Import Body and Param decorators from @nestjs/common
    * Add @Get(":id") decorator to findOne method in EpisodesController
    * Implement findOne method to return one episode response
    * Update create method to accept @Body() input
    * Implement create method to log input and return new episode response

--- src/episodes/episodes.controller.ts
+++ src/episodes/episodes.controller.ts
@@ -1,20 +1,27 @@
-import { Controller, Get, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 
 @Controller('episodes')
 export class EpisodesController {
     @Get()
     findAll(@Query('sort') sort: 'asc' | 'desc' = 'desc' ) {
-        console.log(sort)
-        return "All Episodes"
+        console.log(sort);
+        return "All Episodes";
     }
 
     @Get("featured")
     findFeatured() {
-        return "Featured Episodes"
+        return "Featured Episodes";
+    }
+
+    @Get(":id")
+    findOne(@Param() id: string) {
+        console.log(id);
+        return "One Episode";
     }
 
     @Post()
-    create() {
-        return "New Episodes"
+    create(@Body() input: any ) {
+        console.log(input);
+        return "New Episodes";
     }
 }
