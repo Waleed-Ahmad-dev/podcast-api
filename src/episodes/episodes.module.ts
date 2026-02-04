c6f9beeb9f16f4eb464cbd8f595b67ba3b14db9b
Waleed-Ahmad-dev <itswaleedqureshi@gmail.com>
2026-02-04 11:10:15 +0500

    feat(core): remove unused modules
    * Remove TopicsModule from app.module imports
    * Delete TopicsModule and ConfigModule files
    * Remove ConfigModule from episodes.module imports

--- src/episodes/episodes.module.ts
+++ src/episodes/episodes.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from 'src/config/config.module';
 import { EpisodesController } from './episodes.controller';
 import { EpisodesService } from './episodes.service';
 
 @Module({
-    imports: [ConfigModule],
     controllers: [EpisodesController],
     providers: [EpisodesService]
 })
